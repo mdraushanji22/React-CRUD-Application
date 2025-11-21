205ba3b2691875e6937374a8aab2fda81711fab0
Md Raushan Jilani <90023835+mdraushanji22@users.noreply.github.com>
2025-11-21 22:05:17 +0530

    Add VITE_BASE_URL for API configuration
    Set the VITE_BASE_URL for the application.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+VITE_BASE_URL="https://react-crud-application-api.onrender.com"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
